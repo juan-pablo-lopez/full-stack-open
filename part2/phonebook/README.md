173a3e857034444b64ac7ba05726af638e8cc6b3
Juan Pablo López <jplopezs@gmx.com>
2026-09-20 18:17:18 -0600

    Exercise 2.6

---
+++ part2/phonebook/README.md
@@ -0,0 +1,10 @@
+# Phonebook
+
+This project includes code for **Exercises 2.6 to 2.10 from Part 2**:
+
+a. Forms
+   - Phonebook, step 1
+   - Phonebook, step 2
+   - Phonebook, step 3
+   - Phonebook, step 4
+   - Phonebook, step 5
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+
+  const handleChangeNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAddNewName = (event) => {
+    event.preventDefault();
+    const entryObject = {
+      name: newName,
+      id: String(persons.length + 1)
+    };
+    setPersons(persons.concat(entryObject));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddNewName}>
+        <div>
+          name: <input value={newName} onChange={handleChangeNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        <ul>
+          {
+            persons.map(person => 
+              <li key={person.id}>{person.name}</li>
+            )
+          }
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
