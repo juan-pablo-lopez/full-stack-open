b6a5d73fe5e1273c119fe39f1846931f5a53aa35
Juan Pablo López <jplopezs@gmx.com>
2026-09-21 11:32:48 -0600

    Exercise 2.8

--- part2/phonebook/README.md
+++ part2/phonebook/README.md
@@ -2,7 +2,7 @@
 
 This project includes code for **Exercises 2.6 to 2.10 from Part 2**:
 
-a. Forms
+b. Forms
    - Phonebook, step 1
    - Phonebook, step 2
    - Phonebook, step 3
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,24 +3,33 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleChangeNewName = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleChangeNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const handleAddNewName = (event) => {
     event.preventDefault();
 
+    // There is no indication to validate a new
+    // number exists, so not checking that now.
     if (persons.some(person => person.name === newName)) {
       alert(`'${newName}' is already in the phonebook`);
       return;
     }
 
     const entryObject = {
+      id: String(persons.length + 1),
       name: newName,
-      id: String(persons.length + 1)
+      number: newNumber
     };
     setPersons(persons.concat(entryObject));
+    setNewNumber('');
     setNewName('');
   };
 
@@ -32,6 +41,9 @@ const App = () => {
           name: <input value={newName} onChange={handleChangeNewName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleChangeNewNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -40,7 +52,7 @@ const App = () => {
         <ul>
           {
             persons.map(person => 
-              <li key={person.id}>{person.name}</li>
+              <li key={person.id}>{person.name}: {person.number}</li>
             )
           }
         </ul>
