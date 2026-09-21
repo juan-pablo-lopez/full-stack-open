f90f709c30d90de89b054d2e0e41601cbd3d3ef5
Juan Pablo López <jplopezs@gmx.com>
2026-09-21 12:11:58 -0600

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filterText, setFilterText] = useState('');
+
+  const personsToShow = filterText === ''
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(filterText.toLowerCase()));
+
+  const handleChangeFilterText = (event) => {
+    setFilterText(event.target.value);
+  };
 
   const handleChangeNewName = (event) => {
     setNewName(event.target.value);
@@ -18,7 +32,7 @@ const App = () => {
 
     // There is no indication to validate a new
     // number exists, so not checking that now.
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`'${newName}' is already in the phonebook`);
       return;
     }
@@ -36,22 +50,27 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter: <input value={filterText} onChange={handleChangeFilterText} />
+      </div>
+      <hr />
+      <h2>New entry</h2>
       <form onSubmit={handleAddNewName}>
         <div>
-          name: <input value={newName} onChange={handleChangeNewName} />
+          Name: <input value={newName} onChange={handleChangeNewName} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleChangeNewNumber} />
+          Number: <input value={newNumber} onChange={handleChangeNewNumber} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <hr />
+      <h2>Existing entries</h2>
       <div>
         <ul>
-          {
-            persons.map(person => 
+          { personsToShow.map(person => 
               <li key={person.id}>{person.name}: {person.number}</li>
             )
           }
