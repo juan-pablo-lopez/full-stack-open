f1d3106d4c7e7899fe1ddfc39fba82d808baf2fc
Juan Pablo López <jplopezs@gmx.com>
2026-09-23 13:52:53 -0600

    Exercise 3.2

--- part3/phonebook-backend/src/index.js
+++ part3/phonebook-backend/src/index.js
@@ -32,6 +32,12 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 });
 
+app.get('/info', (request, response) => {
+  const now = new Date();
+  const info = `<p>Phonebook has information for ${persons.length} people.</p><p>${now.toString()}</p>`;
+  response.send(info);
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
